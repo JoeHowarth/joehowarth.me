8a86a2094b0f30508027b17bc262fee88b28f08a
JoeHowarth <josephehowarth@gmail.com>
2017-03-22 15:19:12 -0400

    added type.js

--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@
 $(document).ready(function () {
 
 
-  var toIn = $('.bio').children().first();
-  //  toIn.fadeOut().delay.next().fadeOut();
-
+//  var toIn = $('.bio').children().first();
+//  //  toIn.fadeOut().delay.next().fadeOut();
+//  toIn.siblings().show();
 
   function cycleIn(animTime, interTime) {
     // fades in siblings,
@@ -15,25 +15,40 @@ $(document).ready(function () {
       }, interTime);
     }
   }
-  cycleIn(1000, 1500); // executes above
+  //cycleIn(1000, 1500); // executes above
 
-  function type(string, element) {
-    (function writer(i) {
-      if (string.length <= i++) {
-        element.value = string;
-        return;
-      }
-      element.value = string.substring(0, i);
-      if (element.value[element.value.length - 1] != " ") element.focus();
-      var rand = Math.floor(Math.random() * (100)) + 140;
-      setTimeout(function () {
-        writer(i);
-      }, rand);
-    })(0);
-  }
-  
-  $type("testing", $('#type-area')[0]);
-  alert('past type');
+  $('.bio:first-child').typed({
+    strings: ['Hello World!',
+             'This is Joe Howarth speaking',
+             'hmm, what should I write?',
+             'Well, this is MY site after all, <br> so I\'ll talk a little about myself!',
+             '...'],
+    typeSpeed: 5
+  });
+//  $("#type-area").typed({
+//    strings: ["First sentence."],
+//    typeSpeed: 10
+//  });
+
+
+  //  function type(string, element) {
+  //    (function writer(i) {
+  //      if (string.length <= i++) {
+  //        element.value = string;
+  //        return;
+  //      }
+  //      element.value = string.substring(0, i);
+  //      if (element.value[element.value.length - 1] != " ") element.focus();
+  //      var rand = Math.floor(Math.random() * (100)) + 140;
+  //      setTimeout(function () {
+  //        writer(i);
+  //      }, rand);
+  //    })(0);
+  //  }
+  //  
+  //  var elem = $('#type-area')[0];
+  //  type("testing", elem);
+  //  alert('past type');
 
 
 
