03480e0a84fb7bb63c641c3197eb65ad554e3998
JoeHowarth <josephehowarth@gmail.com>
2017-03-21 22:50:39 -0400

    jquery working, adding jshint

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
+/*jshint jquery: true*/
+$(document).ready(function () {
+  $(".bio").toggle();
 
+
+  var divs = $('bio').children,
+    current = 0;
+
+  myFadeIn($divs.eq(current));
+
+  function myFadeIn(div) {
+    $div.toggle(slow, myFadeIn($div.next()));
+  }
+
+
+
+
+});
