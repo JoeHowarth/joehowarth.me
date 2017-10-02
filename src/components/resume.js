abaa7b1ae8e03e0ebb34a00db3adb47eb8fff62f
JoeHowarth <josephehowarth@gmail.com>
2017-10-02 16:43:11 -0400

    added resume pdf

--- src/components/resume.js
+++ src/components/resume.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import resumePDF from '../assets/resume.pdf';
 import {CenterWrapper, Content} from './prestyled';
+import { Document, Page } from 'react-pdf/build/entry.webpack';
 
 export default class Resume extends React.Component {
 
   render() {
     return (
       <Content>
-        <h1>Wow, what a resume!</h1>
+        {/* <h1>Wow, what a resume!</h1> */}
+        <div className="pdf">
+          <Document file={resumePDF} >
+            <Page pageNumber={1} width={Math.min(800, document.body.clientWidth - 52)} />
+          </Document>
+        </div>
       </Content>
     );
   }
